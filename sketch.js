e4db4b0a8c6175a5ca9ee88916fd0bb0a5191189
Kevin Le <kevin.k.le.2020@gmail.com>
2022-02-05 08:40:07 -0600

    Created (hopefully) all of the HTMl contents. Added styling for a row as well as an uncolored square. Began implementing JS: started implementing grid creation.

---
+++ sketch.js
@@ -0,0 +1,29 @@
+// creates a square and returns it
+function createSquare(side) {
+    let square = document.createElement('div');
+    square.classList.add('noncolored-square');
+    square.style.width = `${side}px`;
+    square.style.height = `${side}px`;
+    return square;
+}
+
+// creates a row with specified row #
+function createRow(num) {
+    let row = document.createElement('div');
+    row.classList.add(`row-${num}`, 'row');
+    return row;
+}
+
+// creates a grid of user chosen size n, NxN
+function createGrid(userSize) {
+    let grid = document.querySelector('.grid-container');
+    for (let i = 0; i < userSize; i++) {
+        grid.appendChild(createRow(i));
+        let row = document.querySelector(`.row-${i}`);
+        for (let j = 0; j < userSize; j++) {
+            row.append(createSquare());
+        }
+    }
+}
+
+createGrid(4)
