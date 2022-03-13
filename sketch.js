05165f0727daebd4950ea4d1397d9dbd284d21ca
Kevin Le <kevin.k.le.2020@gmail.com>
2022-03-13 16:04:10 -0500

    Finished up hover drawing functionality. Began implementing the click-n-drag drawing functionality.

--- sketch.js
+++ sketch.js
@@ -51,36 +51,81 @@ function updateGridFromSlider() {
 }
 
 // previews the size from slider
-function updateSliderPreview(){
+function updateSliderPreview() {
     preview = document.querySelector('.slider-preview');
     preview.textContent = this.value;
 }
 
 // adds functionality to grid sizing
-function gridSizing() {
+function gridSliderSizing() {
     let slider = document.querySelector('#grid-slider');
     let sliderConfirm = document.querySelector('.grid-confirm');
     slider.addEventListener('input', updateSliderPreview); // for when the slider is only being dragged
-    sliderConfirm.addEventListener('click', updateGridFromSlider);
-}
-
-// functionality of the color picker
-function colorFunctionality() {
-    return
+    sliderConfirm.addEventListener('click', updateGridFromSlider); // for when the slider size is confirmed
 }
 
 // click-n-drag listeners to the squares
+function clicknDrag() {
+    color = document.querySelector('#Color');
+    this.style.backgroundColor = color.value;
+    this.classList.add('colored');
+    this.removeEventListener('click', clicknDrag)
+}
 
-// hover listeners to the squres
+// hover listeners to the squares (the default)
+function hoverDrawing() {
+    color = document.querySelector('#Color');
+    // console.log(color);
+    this.style.backgroundColor = color.value;
+    this.classList.add('colored')
+    // console.log(this.classList)
+    this.removeEventListener('mouseover', hoverDrawing);
+}
 
 // adds listeners to the grid so that it can be drawn on
-function addDrawing(grid, color, listener) {
-    return
+function addDrawingListener(coloringListener) {
+    let grid = document.querySelectorAll('.noncolored-square');
+    grid.forEach((square) => {
+        // removing all current listeners
+        square.removeEventListener('mouseover', hoverDrawing);
+        square.removeEventListener('click', clicknDrag);
+        // if the current square is already colored, continue to next square
+        if (!square.classList.contains('colored')) {
+            if (coloringListener == hoverDrawing) {
+                listenerType = 'mouseover';
+            } else if (coloringListener == clicknDrag) {
+                listenerType = 'click';
+            }
+            console.log(listenerType)
+            square.addEventListener(listenerType, coloringListener);
+        } else {
+            return;
+        };
+    });
+}
+
+// adds the hover option functionality
+function addHoverOption() {
+    hoverBtn = document.querySelector('.hover-option');
+    hoverBtn.addEventListener('click', () => {
+        addDrawingListener(hoverDrawing);
+    });
+}
+
+// adds the click-n-drag option functionality
+function addClicknDrag() {
+    dragBtn = document.querySelector('.click-option');
+    dragBtn.addEventListener('click', () => {
+        addDrawingListener(clicknDrag);
+    });
 }
 
 function main() {
-    createGrid(10)
-    gridSizing()
+    createGrid(10);
+    gridSliderSizing(); // functionality of grid slider
+    addDrawingListener(hoverDrawing); // adds the default hover drawing listeners
+    addHoverOption();
+    addClicknDrag();
 }
 
 main()
